92624773ce8767134bc4c0172abe0545dbdc806a
Rudr <148496989+Yu-xo@users.noreply.github.com>
2025-12-28 21:58:49 +0530

    PlayerAsset

--- Scene/Actors/Character/NpC's/np_character.gd
+++ Scene/Actors/Character/NpC's/np_character.gd
@@ -7,7 +7,8 @@ extends Node2D
 @onready var detection_zone: Area2D = $Detection_Zone
 
 
-
+func _physics_process(delta: float) -> void:
+	movement()
 
 func _ready() -> void:
 	detection_zone.body_entered.connect(interaction)
